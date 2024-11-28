bdf43d81a1961f570b5f34dd527edafad771e6d0
jeovabastos <jbastos.im@gmail.com>
2024-11-28 12:33:29 -0400

    feat: BEFORE and NEXT buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [next, setNext] = useState(1)
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>ReferenceIT</h1>
+
+        <img src={`../public/${next}.png`} alt={`${next}.png`}/>
+
+        <button onClick={()=> {
+            if(next === 1){
+              setNext(4)
+              return
+            }
+            setNext(next-1)
+          }}>BEFORE</button>
+          
+        <button onClick={()=> {
+            if(next === 4){
+              setNext(1)
+              return
+            }
+            setNext(next+1)
+          }}>NEXT</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
